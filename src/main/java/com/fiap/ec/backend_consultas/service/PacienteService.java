1dfdb9c0db7431a8607e92233fbb461cb2d35557
fefamolina <molina.fefe@gmail.com>
2026-03-03 22:20:51 -0300

    feat: criação do service de Paciente

--- src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
+++ src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
@@ -1,4 +1,18 @@
 package com.fiap.ec.backend_consultas.service;
-
+import com.fiap.ec.backend_consultas.model.Paciente;
+import com.fiap.ec.backend_consultas.repository.PacienteRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
 public class PacienteService {
+    private final PacienteRepository repository;
+    public PacienteService(PacienteRepository repository) {
+        this.repository = repository;
+    }
+    public Paciente salvar(Paciente paciente) {
+        return repository.save(paciente);
+    }
+    public List<Paciente> listar() {
+        return repository.findAll();
+    }
 }
