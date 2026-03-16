0d8e2029c8726cac0e68c08abbb1dab04507526e
Fernanda Molina <94088509+fefamolina@users.noreply.github.com>
2026-03-16 11:52:08 -0300

    feat: criação da camada service de Especialidade

--- backend_consultas/src/main/java/com/fiap/ec/backend_consultas/controller/Especialidade.java
+++
@@ -1,4 +0,0 @@
-package com.fiap.ec.backend_consultas.controller;
-
-public class Especialidade {
-}
--- backend_consultas/src/main/java/com/fiap/ec/backend_consultas/service/Especialidade.java
+++
@@ -1,4 +0,0 @@
-package com.fiap.ec.backend_consultas.service;
-
-public class Especialidade {
-}
