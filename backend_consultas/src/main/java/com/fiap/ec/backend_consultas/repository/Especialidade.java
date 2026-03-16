9352d4ec56c677742dcfe5d68ace3f3ff3707578
Fernanda Molina <94088509+fefamolina@users.noreply.github.com>
2026-03-16 11:49:48 -0300

    feat: criação do repository de Especialidade

--- backend_consultas/src/main/java/com/fiap/ec/backend_consultas/repository/Especialidade.java
+++ backend_consultas/src/main/java/com/fiap/ec/backend_consultas/repository/Especialidade.java
@@ -1,4 +1,5 @@
 package com.fiap.ec.backend_consultas.repository;
-
-public class Especialidade {
+import com.fiap.ec.backend_consultas.model.Especialidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
 }
